e989ff8e25a3b50f0b48869bdb941f80b4d18db9
EFEELE <ferbock10@gmail.com>
2024-07-16 00:30:56 -0600

    check enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // baseUrl: 'https://sibp.lat/api/'
-    baseUrl: 'http://localhost:3977/api/'
+    baseUrl: 'https://sibp.lat/api/'
+   // baseUrl: 'http://localhost:3977/api/'
 
 };
